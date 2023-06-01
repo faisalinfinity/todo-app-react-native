7ec65eccc92f82cf66d8d1f831bef12aa76cb086
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-06-01 14:14:09 +0530

    completed  basic func

--- App.tsx
+++ App.tsx
@@ -5,116 +5,172 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import {
-  Button,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
+  FlatList,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({ children, title }: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   useEffect(() => {
+    loadTodos();
+  }, []);
+
+  useEffect(() => {
+    saveTodos();
+  }, [todos]);
+
+  const loadTodos = async () => {
+    try {
+      const storedTodos = await AsyncStorage.getItem(STORAGE_KEY);
+      if (storedTodos !== null) {
+        setTodos(JSON.parse(storedTodos));
+      }
+    } catch (error) {
+      console.log('Error loading todos', error);
+    }
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step Two">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-         
-              <Button title='Edit'></Button>    <Button title='Delete'></Button>
-           
+  const saveTodos = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+    } catch (error) {
+      console.log('Error saving todos', error);
+    }
+  };
 
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next
-          </Section>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+  const addTodo = () => {
+    if (newTodo.trim() === '') {
+      return; // Don't add empty todos
+    }
+    const todo = { id: Date.now().toString(), text: newTodo, completed: false };
+    setTodos((prevTodos) => [...prevTodos, todo]);
+    setNewTodo('');
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const renderTodoItem = ({ item }) => (
+    <View style={styles.todoItem}>
+      <TouchableOpacity onPress={() => toggleTodo(item.id)}>
+        <Text style={[styles.todoText, item.completed && styles.completedTodoText]}>
+          {item.text}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => deleteTodo(item.id)}>
+        <Text style={styles.deleteButton}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+    <Text style={styles.title}>Todo App</Text>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter a new todo"
+        value={newTodo}
+        onChangeText={(text) => setNewTodo(text)}
+        onSubmitEditing={addTodo}
+      />
+      <TouchableOpacity style={styles.addButton} onPress={addTodo}>
+        <Text style={styles.addButtonLabel}>Add</Text>
+      </TouchableOpacity>
+    </View>
+    <FlatList
+      data={todos}
+      renderItem={renderTodoItem}
+      keyExtractor={(item) => item.id}
+      style={styles.todoList}
+    />
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
   },
-  sectionTitle: {
+  title: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    flex: 1,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  addButtonLabel: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  todoList: {
+    marginTop: 10,
+  },
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  todoText: {
+    fontSize: 16,
+  },
+  completedTodoText: {
+    textDecorationLine: 'line-through',
+  },
+  deleteButton: {
+    color: 'red',
+    fontWeight: 'bold',
   },
 });
 
